fca4e3b731405b8ca557efdd77ae0850f820e9c1
jesusburrueco <86404445+jesusburrueco@users.noreply.github.com>
2025-05-20 17:24:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def index():
 
     return render_template("index.html", category=selected_category, result=result)
 
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
